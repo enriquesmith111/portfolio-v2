b9a436762caf293ccc78c1244816d6c691db0a5d
Enrique Smith Dean <enriquesmith111@gmail.com>
2024-05-25 22:59:15 -0300

    changed css animation-timeline ofr js code on banner animation

--- src/Banner.js
+++ src/Banner.js
@@ -3,6 +3,22 @@ import BannerText from './BannerText'
 
 
 export default function Banner() {
+    window.addEventListener('load', function () {
+        const banner = document.querySelector('.banner h1');
+        banner.classList.add('in-view');
+
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    banner.classList.add('in-view');
+                } else {
+                    banner.classList.remove('in-view');
+                }
+            });
+        });
+
+        observer.observe(banner);
+    });
 
     return (
         <div className='banner'>
