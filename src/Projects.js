7fab2ddfa72fdad8824e9e08766b7b339da29afd
Enrique Smith Dean <enriquesmith111@gmail.com>
2024-06-04 19:25:19 -0300

    added links to projects

--- src/Projects.js
+++ src/Projects.js
@@ -7,6 +7,7 @@ const projectData = [
         name: '</Earth Roulette>',
         description: 'Earth Roulette is a web application built with React.js that injects a touch of serendipity into travel planning. It utilizes various APIs to present users with a randomly chosen country, enticing them to explore new destinations. This project demonstrates my proficiency in React.js, API integration, and creating a user-friendly web application. Earth Roulette serves as a testament to my ability to leverage technology to promote exploration and facilitate travel planning, further enhanced by the engaging animations created with LottieFiles.',
         image: 'empty-laptop.png',
+        link: 'https://enriquesmith111.github.io/EarthRoulette/',
         index: 0,
         key: 'earth-roulette'
     },
@@ -14,15 +15,16 @@ const projectData = [
         name: '</Mokepon>',
         description: `Mokepon is a project that showcases my newly acquired skills on Javascript coding for creating a simple yet fun clone of the popular game franchise Pokemon, in this game you can play against a single opponent or  multiplayer 2v2. Mokepon's JavaScript code provides direct access to the browser's Document Object Model, enabling immediate and seamless updates to the displayed graphics and interactions. This eliminates the need for intermediary libraries or frameworks, minimizing the overhead and enhancing overall performance. By utilizing JavaScript directly, Mopekon delivers a smooth and responsive gaming experience across a wide range of devices, including mobile devices.`,
         image: 'empty-laptop.png',
+        link: 'https://enriquesmith111.github.io/mokepon/',
         index: 1,
         key: 'mokepon'
     },
     {
         name: '</Photography Blog>',
         description: `Built a photography blog using React.js to showcase a portfolio of travel photographs and integrate a seamless e-commerce experience for print sales.
-
         Leveraged React's component-based architecture to create a dynamic and responsive user interface for displaying images and processing purchase transactions. Implemented a dedicated blog section to share travel stories and engage with a growing audience. This project demonstrates proficiency in React.js for building interactive web applications with e-commerce functionality.`,
         image: 'empty-laptop.png',
+        link: 'http://localhost:3001/portfolio-v2#project-container',
         index: 2,
         key: 'blog'
     },
@@ -30,6 +32,7 @@ const projectData = [
         name: '</Sea&B>',
         description: `Sea&B is a Ruby on Rails web app modeled after AirBnB, tailored for luxury yacht rentals. By leveraging Ruby, PostgreSQL for the database, and the MVC architecture, Sea&B mirrors the complexity of AirBnB's schema. Hosting on Heroku ensures seamless deployment and scalability. Bootstrap enhances the user interface, enabling intuitive navigation and booking. Additionally, the integration of Mapbox API facilitates location-based yacht searches. Offering a diverse selection of sailboats, motor yachts, and catamarans, Sea&B stands out with its user-friendly interface, robust database structure, and advanced search functionality. Perfect for holidaymakers seeking luxury yacht rentals or owners looking to monetize their vessels, Sea&B is a tech-forward solution in the yacht rental industry.`,
         image: 'empty-laptop.png',
+        link: 'https://github.com/Ahme097/SEA_AND_B',
         index: 3,
         key: 'sea-and-b'
     },
@@ -37,6 +40,7 @@ const projectData = [
         name: '</Fit-Squad>',
         description: ' My first ever programming project is Fit-Squad is a Ruby on Rails web app fostering a fitness community. It stands out by emphasizing social connections and group events. Users engage with like-minded individuals, join sports groups, and attend activities. The app also offers an AI model for personalized home workouts, catering to users fitness goals efficiently. Utilizing PostgreSQL, Mapbox API, and OpenAI API, Fit-Squad ensures a well-structured and maintainable codebase. This blend of social interaction and AI-driven fitness guidance sets Fit-Squad apart in the realm of fitness apps, promoting support, motivation, and convenience for users seeking to improve their fitness levels.',
         image: 'empty-laptop.png',
+        link: 'https://github.com/enriquesmith111/Fit-Squad',
         index: 4,
         key: 'fit-squad'
     },
@@ -99,7 +103,7 @@ function ProjectText({ projectObj }) {
         <div id='project-text' className='project-text'>
             <div className='text-and-link'>
                 <TextRandomizer initialText={projectObj.name} />
-                <i className='fa-solid fa-up-right-from-square pulse-animation' id='vibrate'></i>
+                <a href={projectObj.link} target="_blank" class="fa-solid fa-up-right-from-square" id='vibrate' rel="noreferrer"></a>
             </div>
             <p>{projectObj.description}</p>
         </div>
@@ -114,7 +118,7 @@ function Projectimage({ projectObj }) {
         entries.forEach((entry) => {
 
 
-            if (entry.isIntersecting && projectObj.index === 1) {
+            if (entry.isIntersecting && projectObj.key === 'mokepon') {
                 entry.target.classList.add('show-sprite')
             } else {
                 entry.target.classList.remove('show-sprite')
