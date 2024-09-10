3c341eb4f79ebb8eed5a387a9700dcc01d0ce097
Enrique Smith Dean <enriquesmith111@gmail.com>
2024-09-10 16:46:20 -0300

    test image fix

--- src/Certificates.js
+++ src/Certificates.js
@@ -5,28 +5,28 @@ import "./certificates.css"
 const certificateData = [
     {
         name: '</Udemy Javascript 0 to Expert>',
-        description: `I successfully completed the 80-hour Jonas Schmedtmann JavaScript 0 to Expert course. This comprehensive program thoroughly covered the fundamentals of JavaScript programming, from basic concepts to advanced topics. Throughout the course, I gained hands-on experience by building practical projects, which allowed me to solidify my understanding and apply my newfound knowledge in real-world scenarios.`,
+        description: `2024 - I successfully completed the 80-hour Jonas Schmedtmann JavaScript 0 to Expert course. This comprehensive program thoroughly covered the fundamentals of JavaScript programming, from basic concepts to advanced topics. Throughout the course, I gained hands-on experience by building practical projects, which allowed me to solidify my understanding and apply my newfound knowledge in real-world scenarios.`,
         index: 0,
         image: 'javascript.png',
         key: 'javascript'
     },
     {
         name: '</British Computer Science>',
-        description: 'I earned a Web Development Bootcamp certificate accredited by the British Computer Society (BCS). This prestigious certification validates my skills and expertise in web development, confirming that I have met the rigorous standards set by a globally recognized professional body.',
+        description: '2023 / 2024 - I earned a Web Development Bootcamp certificate accredited by the British Computer Society (BCS). This prestigious certification validates my skills and expertise in web development, confirming that I have met the rigorous standards set by a globally recognized professional body.',
         index: 1,
         image: 'british-computer-science.png',
         key: 'british-computer-science'
     },
     {
         name: '</Le Wagon Web Development>',
-        description: 'I obtained a Web Development Bootcamp certificate from Le Wagon, a renowned coding school. During my 8-month intensive program in London, I delved into the intricacies of web development, mastering Ruby and Ruby on Rails within the MVC framework. Additionally, I acquired foundational JavaScript skills for DOM manipulation. To reinforce my understanding and simulate real-world development environments, I collaborated with peers to create robust web applications.',
+        description: '2023 / 2024 - I obtained a Web Development Bootcamp certificate from Le Wagon, a renowned coding school. During my 8-month intensive program in London, I delved into the intricacies of web development, mastering Ruby and Ruby on Rails within the MVC framework. Additionally, I acquired foundational JavaScript skills for DOM manipulation. To reinforce my understanding and simulate real-world development environments, I collaborated with peers to create robust web applications.',
         index: 2,
         image: 'web-development.png',
         key: 'web-development'
     },
     {
         name: '</Platzi Front-end Developer>',
-        description: `I earned a Front-End Developer certificate from Platzi, a leading South American online education platform. As a beginner, this course provided me with a solid foundation in coding, enabling me to develop basic single-page applications. I honed my skills in semantic HTML, CSS, and Bootstrap, mastering the art of crafting visually appealing and pixel-perfect user interfaces.`,
+        description: `2023 - I earned a Front-End Developer certificate from Platzi, a leading South American online education platform. As a beginner, this course provided me with a solid foundation in coding, enabling me to develop basic single-page applications. I honed my skills in semantic HTML, CSS, and Bootstrap, mastering the art of crafting visually appealing and pixel-perfect user interfaces.`,
         index: 3,
         image: 'front-end.png',
         key: 'front-end'
